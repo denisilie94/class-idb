8e1902a986268ee30571ceb279cf9c3fdf2a00a8
dia <denisilie94@gmail.com>
2023-05-23 23:01:39 +0300

    Added datasets details

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
-# class-idb
+# Classification with dictionary learning and a distance barrier promoting incoherence (class-idb)
+
+
+## Datasets
+
+Most of the datasets are available [here](http://www.zhuolin.umiacs.io/projectlcksvd.html).
+
+The action bank features can be downloaded from [here](https://cse.buffalo.edu/~jcorso/r/actionbank/).
+
+We provide some extractors to organize the *HMDB51* and *UCF50* datasets as mat files efficiently.
+
+## Reproducing the results
+
+The proposed methods are available under the following scripts: *aksvd_idb_test.m* and *aksvd_itdb_test.m*.
---
+++ extractors/hmdb51_extractor.m
@@ -0,0 +1,39 @@
+% This script is used to extract features from the
+% original repository of action bank from Buffalo University.
+
+clc;
+clear;
+
+k = 5000; % number of pca features
+dirFolders = dir('../data/ab_hmdb51_e2f1g2_matlab');
+
+position = 1;
+label = 1;
+db_features = zeros(14965, 6766);
+
+for i = 1:length(dirFolders)
+   if ~strcmp(dirFolders(i).name, '.') && ~strcmp(dirFolders(i).name, '..') && dirFolders(i).isdir
+       files = dir(strcat(dirFolders(i).folder, '/', dirFolders(i).name, '/*.mat'));
+       
+       for j = 1:length(files)
+            tmp_data = load(strcat(files(j).folder, '/', files(j).name));
+            db_features(:, position) = tmp_data.v;
+            db_labels(position) = label;
+            position = position + 1;
+       end
+       
+       label = label + 1;
+   end
+end
+
+[coeff, ~, ~] = pca(db_features');
+featureMat = (db_features' * coeff(:, 1:k))';
+n_samples = size(db_features, 2);
+n_classes = length(unique(db_labels));
+labelMat = zeros(n_classes, n_samples);
+
+for i = 1:n_samples
+    labelMat(db_labels(i), i) = 1;
+end
+
+save('hmdb51_dataset.mat', 'featureMat', 'labelMat');
